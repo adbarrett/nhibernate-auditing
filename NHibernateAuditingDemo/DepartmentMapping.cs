a9a9c06db1ea979fe608eb2960325242940b621c
Amy Barrett <a.d.barrett@outlook.com>
2019-02-16 21:05:56 +0000

    Added employee and department mappings

---
+++ NHibernateAuditingDemo/DepartmentMapping.cs
@@ -0,0 +1,15 @@
+﻿namespace NHibernateAuditingDemo
+{
+    using FluentNHibernate.Mapping;
+
+    public class DepartmentMapping : ClassMap<Department>
+    {
+        public DepartmentMapping()
+        {
+            Table("Departments");
+            Id(x => x.Id);
+            Map(x => x.Name);
+            HasMany(x => x.Employees);
+        }
+    }
+}
---
+++ NHibernateAuditingDemo/EmployeeMapping.cs
@@ -0,0 +1,17 @@
+﻿namespace NHibernateAuditingDemo
+{
+    using FluentNHibernate.Mapping;
+
+    public class EmployeeMapping : ClassMap<Employee>
+    {
+        public EmployeeMapping()
+        {
+            Table("Employees");
+            Id(x => x.Id);
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+            Map(x => x.DateOfBirth);
+            References(x => x.Department);
+        }
+    }
+}
