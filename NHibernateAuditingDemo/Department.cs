5bed3cef1f2732b877a7afaf2f023af16cc9ccae
Amy Barrett <a.d.barrett@outlook.com>
2019-02-16 21:03:38 +0000

    Added employee and department classes

---
+++ NHibernateAuditingDemo/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace NHibernateAuditingDemo
+{
+    using System.Collections.Generic;
+
+    public class Department
+    {
+        public virtual string Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IList<Employee> Employees { get; set; }
+    }
+}
---
+++ NHibernateAuditingDemo/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace NHibernateAuditingDemo
+{
+    using System;
+
+    public class Employee
+    {
+        public virtual string Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
